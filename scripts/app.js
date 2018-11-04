c53e18208b1c272582f64a9076e4c60b02fc9f9c
Kenny <kennethichoi@gmail.com>
2018-11-03 23:19:51 -0700

    Interactive cube display added

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,41 @@
-// Set variables
-let container = document.querySelector('.container');
-let inner = document.querySelector('.perspective');
+// Cube
 
-// Set event listeners
-// container.addEventListener('mousein', mouseIn);
-// container.addEventListener('mouseout', mouseOut);
-container.addEventListener('mousemove', mouseMove);
+// DOM Elements
+let buttons = document.querySelectorAll('.cube-btn');
+let cube = document.querySelector('.cube');
+let showFront = document.querySelector('.show-front');
+let showBack = document.querySelector('.show-back');
+let showRight = document.querySelector('.show-right');
+let showLeft = document.querySelector('.show-left');
+let showTop = document.querySelector('.show-top');
+let showBottom = document.querySelector('.show-bottom');
 
-// Set functions
-let counter = 0;
-let updateRate = 10;
-let isTimeToUpdate = function() {
-    return counter++ % updateRate === 0;
-} 
+// Event Listeners
+buttons.forEach(button => {
+    button.addEventListener('click', changeCube);
+});
 
-function mouseMove(e) {
-    if(isTimeToUpdate()) {
-        console.log(update(event));
+// Cube manipulation
+function changeCube(e) {
+    // console.log(e.target.classList);
+
+    if(e.target.classList.contains('show-front')) {
+        cube.classList = 'cube';
+        cube.classList.toggle('show-front');
+    } else if(e.target.classList.contains('show-back')) {
+        cube.classList = 'cube';
+        cube.classList.toggle('show-back');
+    } else if(e.target.classList.contains('show-left')) {
+        cube.classList = 'cube';
+        cube.classList.toggle('show-left');
+    } else if(e.target.classList.contains('show-right')) {
+        cube.classList = 'cube';
+        cube.classList.toggle('show-right');
+    } else if(e.target.classList.contains('show-top')) {
+        cube.classList = 'cube';
+        cube.classList.toggle('show-top');
+    } else if(e.target.classList.contains('show-bottom')) {
+        cube.classList = 'cube';
+        cube.classList.toggle('show-bottom');
     }
 }
