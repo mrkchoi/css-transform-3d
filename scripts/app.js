af3a3d16075d7e69e71cee202045d3c24de67ab5
Kenny <kennethichoi@gmail.com>
2018-11-04 17:09:45 -0800

    Added vanilla tilt, perspective change on mouse move

--- scripts/app.js
+++ scripts/app.js
@@ -94,3 +94,37 @@ function rotateCarousel(e) {
         carousel.style.transform = `translateZ(-41.21rem) rotateY(${degree}deg)`;
     }
 }
+
+
+
+
+/**************** 
+// Mousemove Perspective
+****************/
+
+// {
+//     reverse:                false,  // reverse the tilt direction
+//     max:                    35,     // max tilt rotation (degrees)
+//     perspective:            1000,   // Transform perspective, the lower the more extreme the tilt gets.
+//     scale:                  1,      // 2 = 200%, 1.5 = 150%, etc..
+//     speed:                  300,    // Speed of the enter/exit transition
+//     transition:             true,   // Set a transition on enter/exit.
+//     axis:                   null,   // What axis should be disabled. Can be X or Y.
+//     reset:                  true    // If the tilt effect has to be reset on exit.
+//     easing:                 "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+//     glare:                  false   // if it should have a "glare" effect
+//     "max-glare":            1,      // the maximum "glare" opacity (1 = 100%, 0.5 = 50%)
+//     "glare-prerender":      false,  // false = VanillaTilt creates the glare elements for you, otherwise
+//                                     // you need to add .js-tilt-glare>.js-tilt-glare-inner by yourself
+//     "mouse-event-element":  null    // css-selector or link to HTML-element what will be listen mouse events
+// }
+
+VanillaTilt.init(document.querySelector('.mouse__card'), {
+    max: 25,
+    reverse: true,
+    speed: 10000,
+    perspective: 2000,
+    transition: true,
+    glare: true,
+    'max-glare': .2
+});
